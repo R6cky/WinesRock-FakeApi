cd9009746e136d7a24a46b36d9bc1173681ce4f2
r6cky <cleitonrocha321@hotmail.com>
2023-01-17 21:41:52 -0300

    fix: remove trash data

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
-  donation: 644,
-  request: 644,
-  getdonation: 644,
-  getrequest: 644
+  products: 644,
 });
 
 app.use(cors());
